f9071eacb5eafb4e68a2e49838b8cd18f073d0ec
HFUG0918 <hfelipeurueta@gmail.com>
2023-01-30 14:30:37 -0500

    Tarjeta ultimo cambio

--- modelo/vista/contratista.php
+++ modelo/vista/contratista.php
@@ -5,6 +5,7 @@ include("../controlador/empleadoController.php");
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,34 +14,29 @@ include("../controlador/empleadoController.php");
     <title>Contratista</title>
 </head>
 
-<body >
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Calculardora</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Planta</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    <div class="container mt-5 d-flex justify-content-center">
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Calculardora</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Planta</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-1 d-flex justify-content-center">
         <div class="card d-flex border border-primary" style="width: 50rem;">
             <div class="card-body ">
-                <h4><?php
-                        echo "Calcular Salario Empleado Contratista : ";
-                        echo "<br>";
-                        
-                ?></h4> 
-               
+                <h4>Calcular Salario Empleado Contratista</h4>
                 <?php
                 $empleado = new EmpleadoController();
 
@@ -72,31 +68,35 @@ include("../controlador/empleadoController.php");
                 $objContratista->setTotalHoras($totalHorasTrabajadas);
                 //imprimimos datos de entrada
                 ?>
-                <div class="form group"> 
-                    <label for="">Id Empleado</label>
-                <?php
-                echo $id_empleado;
-                echo "<br> <br>Nombres Empleado: " . $nombre_empleado . " " . $apellido_empleado;
-                echo "<br> <br>Cargo Empleado: " . $tipo_empleado;
-                echo "<br> <br>Valor de la Hora: $" . $objContratista->getValorHora();
-                echo "<br> <br>Total Horas Trabajas en el Mes: " . $objContratista->getTotalHoras();
-                ?>
+                <div class="form group">
+                    <label for="idEmpleado">Id Empleado:</label> <br>
+                    <input type="text" class="form-control" placeholder=<?php echo $id_empleado ?> disabled>
+                    <label for="idEmpleado">Nombres Empleado:</label> <br>
+                    <input type="text" class="form-control" placeholder="<?php echo $nombre_empleado . ' ' . $apellido_empleado ?>" disabled>
+                    <label for="idEmpleado">Cargo Empleado:</label> <br>
+                    <input type="text" class="form-control" placeholder=<?php echo $tipo_empleado ?> disabled>
+                    <label for="idEmpleado">Valor de la Hora:</label> <br>
+                    <input type="text" class="form-control" placeholder=<?php echo $objContratista->getValorHora() ?> disabled>
+                    <label for="idEmpleado">Total Horas Trabajas en el Mes:</label> <br>
+                    <input type="text" class="form-control" placeholder=<?php echo $objContratista->getTotalHoras() ?> disabled>
+
                 </div>
                 <?php
                 //calculam os el salario del empleado de Contrato
-                $objContratista->calcularSalario(4000, $totalHorasTrabajadas);?>
-                                <h4><?php
-                        echo " <br> Resultados: <br>";
-                        
-                ?></h4>
-                <?php
+                $objContratista->calcularSalario(4000, $totalHorasTrabajadas); ?>
+                <h4><br> Resultados: <br></h4>
 
-                //Mostramos resultados
-                echo "<br><br>Id Empleado: " . $id_empleado;
-                echo "<br><br>Nombres Empleado: " . $nombre_empleado . " " . $apellido_empleado;
-                echo "<br><br>Cargo Empleado: " . $tipo_empleado;
-                echo "<br><br>Salario Neto a recibir en el Mes: " . $objContratista->getSalario();
-                ?>
+                <div class="form group">
+                    <label for="idEmpleado">Id Empleado:</label> <br>
+                    <input type="text" class="form-control" placeholder=<?php echo $id_empleado ?> disabled>
+                    <label for="idEmpleado">Nombres Empleado:</label> <br>
+                    <input type="text" class="form-control" placeholder="<?php echo $nombre_empleado . ' ' . $apellido_empleado ?>" disabled>
+                    <label for="idEmpleado">Cargo Empleado:</label> <br>
+                    <input type="text" class="form-control" placeholder=<?php echo $tipo_empleado ?> disabled>
+                    <label for="idEmpleado">Salario Neto a recibir en el Mes:</label> <br>
+                    <input type="text" class="form-control" placeholder=<?php echo $objContratista->getSalario() ?> disabled>
+
+                </div>
             </div>
         </div>
     </div>
